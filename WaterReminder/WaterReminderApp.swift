479f5442f08f2976cd383281c3104b9c0487e710
Stacey Smith <stacey@appsdevo.com>
2024-03-20 14:31:58 -0600

    Initial Commit

---
+++ WaterReminder/WaterReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterReminderApp.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WaterReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
