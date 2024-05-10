87c775887eba7597bf5b08c004e30f417f7f2a0e
Stacey Smith <stacey@appsdevo.com>
2024-05-10 11:03:52 -0600

    Add Alarm Row

---
+++ WaterReminder/AddReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  addReminderView.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 5/10/24.
+//
+
+import SwiftUI
+
+struct addReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addReminderView()
+}
---
+++ WaterReminder/AlarmRow.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmRow.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 5/10/24.
+//
+
+import SwiftUI
+
+struct AlarmRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlarmRow()
+}
