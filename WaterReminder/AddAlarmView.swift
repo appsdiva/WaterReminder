916908ac6c46c5d5d2bddc2245010b10101151de
Stacey Smith <stacey@appsdevo.com>
2024-05-10 16:18:43 -0600

    fix add alarm view

---
+++ WaterReminder/AddAlarmView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAlarmView.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 5/10/24.
+//
+
+import SwiftUI
+
+struct AddAlarmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAlarmView()
+}
