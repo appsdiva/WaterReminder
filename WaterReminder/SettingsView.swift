3511e3a421b1118112f27064aa1fe26e310e37cd
Stacey Smith <stacey@appsdevo.com>
2024-05-16 10:44:03 -0600

    add settings view

---
+++ WaterReminder/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 5/16/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
