7fb40fd33d231453d2f4acd03c28230dc89d42a3
Stacey Smith <stacey@appsdevo.com>
2024-06-13 15:41:24 -0600

    Add TermsOfUse page

---
+++ WaterReminder/TermsofUseView.swift
@@ -0,0 +1,18 @@
+//
+//  TermsofUseView.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 6/13/24.
+//
+
+import SwiftUI
+
+struct TermsofUseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermsofUseView()
+}
