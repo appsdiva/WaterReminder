3d11a0df812947e17756bdc0d7356a4fd6cedf36
Stacey Smith <stacey@appsdevo.com>
2024-06-13 15:55:24 -0600

    Add TipView

---
+++ WaterReminder/TipView.swift
@@ -0,0 +1,18 @@
+//
+//  TipView.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 6/13/24.
+//
+
+import SwiftUI
+
+struct TipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TipView()
+}
