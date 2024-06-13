f4cf358c2107e445d966f2e923c989628037af79
Stacey Smith <stacey@appsdevo.com>
2024-06-13 15:32:19 -0600

    Add SettingsView

---
+++ WaterReminder/PrivacyPolicyView.swift
@@ -0,0 +1,18 @@
+//
+//  PrivacyPolicyView.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 6/13/24.
+//
+
+import SwiftUI
+
+struct PrivacyPolicyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrivacyPolicyView()
+}
