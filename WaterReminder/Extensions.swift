c309d6615abee0bd25910d135b05254916e2e561
Stacey Smith <stacey@appsdevo.com>
2024-06-13 17:18:38 -0600

    Fix StoreManager issue

---
+++ WaterReminder/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 6/13/24.
+//
+
+import Foundation
---
+++ WaterReminder/StoreManager.swift
@@ -0,0 +1,8 @@
+//
+//  StoreManager.swift
+//  WaterReminder
+//
+//  Created by Stacey Smith on 6/13/24.
+//
+
+import Foundation
